d72f3997312f75fd988ba91f3531f2f009880b1e
aldyckybw <bagusaldycky@gmail.com>
2023-01-31 12:56:01 +0700

    finished add swagger to express

--- src/routes.ts
+++ src/routes.ts
@@ -23,8 +23,50 @@ import { createSessionSchema } from "./schemas/session.schema";
 import { createUserSchema } from "./schemas/user.schema";
 
 function routes(app: Express) {
+  /**
+   * @openapi
+   * tags:
+   *   name: HealthCheck
+   *   description:
+   * /healthcheck:
+   *   get:
+   *     tags: [HealthCheck]
+   *     summary:
+   *     description: Responds if the app is up and running
+   *     responses:
+   *       200:
+   *         description: App is up and running.
+   */
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
+  /**
+   * @openapi
+   * tags:
+   *   name: User
+   *   description:
+   * /api/users:
+   *   post:
+   *     tags: [User]
+   *     summary:
+   *     description: Register a User
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/CreateUserInput'
+   *     responses:
+   *       200:
+   *         description: Success.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/CreateUserResponse'
+   *       409:
+   *         description: Conflict.
+   *       400:
+   *         description: Bad Request.
+   */
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
   app.post(
@@ -49,6 +91,31 @@ function routes(app: Express) {
     updateProductHandler
   );
 
+  /**
+   * @openapi
+   * tags:
+   *   name: Product
+   *   description:
+   * /api/products/{productId}:
+   *   get:
+   *     tags: [Product]
+   *     summary:
+   *     description: Get a single product by the productId
+   *     parameters:
+   *       - name: productId
+   *         in: path
+   *         description: The id of the product
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Success.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schema/Product'
+   *       404:
+   *         description: Product not found.
+   */
   app.get(
     "/api/products/:productId",
     validateResource(getProductSchema),
